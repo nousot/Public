289e69d4881f095f79f3bee4766899e96e69560f
baogorek <baogorek@github.com>
2020-08-05 08:36:00 -0500

    Update blog article and Kaggle README

--- blog/simulate-transformation-bias.R
+++ blog/simulate-transformation-bias.R
@@ -4,33 +4,42 @@ set.seed(543)
 
 # Generate Poisson data ------------------------------------------------------
 T <- 100
-mu <- .20  # Change this from .20 to 20 
+mu <- 20  # Change this from .20 to 20 
 sim_df <- data.frame(t=1:T, y=rpois(T, mu), type='Y_t')
 
 
-# Local Mean (loess) model on the original scale
+# Local Mean (loess) model on the original scale -----------------------------
 mod_orig <- loess(y ~ t, data=sim_df)
 sim_df_loess <- sim_df
 sim_df_loess$y <- predict(mod_orig)
 sim_df_loess$type <- 'hat E(Y_t)'
 
 
-# Local Mean (loess) model on the sqrt scale
+# Local Mean (loess) model on the sqrt scale ---------------------------------
 mod_tr <- loess(sqrt(y) ~ t, data=sim_df)
 sim_df_tr_loess <- sim_df
 sim_df_tr_loess$y <- predict(mod_tr) ^ 2
 sim_df_tr_loess$type <- 'hat E(sqrt(Y_t)) ^ 2'
 
 
-# Analysis of bias on the square root scale
-expected_bias <- -1 / (8 * sqrt(mu)) + 1 / (16 * mu ^ (1.5))
-observed_bias <- mean(sqrt(sim_df_tr_loess$y) - sqrt(sim_df_loess$y))
+# Analysis of bias on the square root scale ----------------------------------
+expected_bias_sqrt <- -1 / (8 * sqrt(mu)) + 1 / (16 * mu ^ (1.5))
+observed_bias_sqrt <- mean(sqrt(sim_df_tr_loess$y) - sqrt(sim_df_loess$y))
 cat("On the square root scale, a Taylor expansion estimates a bias of",
+    expected_bias_sqrt, "\nwhereas the mean difference in loess estimates",
+    "(on the sqrt scale) is", observed_bias_sqrt, "\n")
+
+# Analysis of bias on the original scale --------------------------------------
+E_sqrt <- sqrt(mu) + expected_bias_sqrt
+E_reconstituted <- E_sqrt ^ 2
+expected_bias <- E_reconstituted - mu
+observed_bias <- mean(sim_df_tr_loess$y - sim_df_loess$y)
+cat("On the original scale, a Taylor expansion estimates a bias of",
     expected_bias, "\nwhereas the mean difference in loess estimates",
-    "(on the sqrt scale) is", observed_bias, "\n")
+    "is", observed_bias, "\n")
 
+# Plotting --------------------------------------------------------------------
 plot_df <- rbind(sim_df, sim_df_loess, sim_df_tr_loess)
-
 p <- ggplot(plot_df, aes(x=t, y=y, color=type)) +
   geom_point() +
   xlab("time (t), the sequential order of observed data") +
